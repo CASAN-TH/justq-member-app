a604f83bef20c35bc0b42fe3b1caed24a2da355f
KrittinTch <petesuper256@gmail.com>
2020-05-29 15:55:49 +0700

    time modal send data

--- src/app/shop/queue-time/queue-time.component.ts
+++ src/app/shop/queue-time/queue-time.component.ts
@@ -1,5 +1,6 @@
 import { ShopService } from './../../services/shop.service';
 import { Component, OnInit, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-queue-time',
@@ -12,13 +13,17 @@ export class QueueTimeComponent implements OnInit {
   @Input() dayNumber: any;
 
   openTimeData: any;
+  selected = {
+    que: ""
+  };
 
   constructor(
-    private Shopservice: ShopService
+    private Shopservice: ShopService,
+    public modalController: ModalController
   ) { }
 
   ngOnInit() {
-    this.Shopservice.getOpenTime().then((res) =>{
+    this.Shopservice.getOpenTime().then((res) => {
       // console.log(res);
       this.openTimeData = res.data;
       console.log(this.openTimeData);
@@ -27,8 +32,18 @@ export class QueueTimeComponent implements OnInit {
     console.log(this.dayNumber);
   }
 
-  clickTime(){
-    console.log("click time");
+  clickTime(item) {
+    this.selected = item;
+    console.log(item);
+  }
+
+  saveTime(){
+    const body = {
+      queTime: this.selected
+    };
+    console.log(body);
+    this.modalController.dismiss(body);
+
   }
 
 }
